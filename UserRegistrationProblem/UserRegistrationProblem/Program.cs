a79d8951aaed117f5cc681239a737b25f5e259f0
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-05-29 22:55:45 +0530

    [check first name true or false]

--- UserRegistrationProblem/UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/UserRegistrationProblem/Program.cs
@@ -7,6 +7,15 @@ namespace UserRegistrationProblem
         public static void Main(string[] args) 
         {
             Console.WriteLine("welcome to UserRegistrationProblem day 24 assignment");
+
+
+            
+            Console.WriteLine("Enter the first name = ");
+            
+            UserRegistrationRegex userRegistrationRegex = new UserRegistrationRegex();
+
+            string firstName = Console.ReadLine();
+            Console.WriteLine(userRegistrationRegex.validateFirstName(firstName));
         }
     }
 }
---
+++ UserRegistrationProblem/UserRegistrationProblem/UserRegistrationRegex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace UserRegistrationProblem
+{
+    class UserRegistrationRegex
+    {
+        public static string VALIDNAMEPATTERN = "^[A-Z]{1}[a-z]{2,}$";
+        public bool validateFirstName(String firstName)
+        {
+            return Regex.IsMatch(firstName, VALIDNAMEPATTERN);
+        }
+    }
+}
