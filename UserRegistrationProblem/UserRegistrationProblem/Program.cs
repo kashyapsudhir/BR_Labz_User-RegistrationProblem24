827883ad325e5407ff23c3ef2b6b5265cd2f2ca1
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-05-29 23:07:37 +0530

    [check email true or false by regex expression]

--- UserRegistrationProblem/UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/UserRegistrationProblem/Program.cs
@@ -8,13 +8,22 @@ namespace UserRegistrationProblem
         {
             Console.WriteLine("welcome to UserRegistrationProblem day 24 assignment");
 
-            Console.WriteLine("Enter the first name = ");
-            string firstName = Console.ReadLine();
-            Console.WriteLine("Enter the last name = ");
-            string lastName = Console.ReadLine();
+            //UC-1
+            //Console.WriteLine("Enter the first name = ");
+            //string firstName = Console.ReadLine();
+
+            //UC-2
+            //Console.WriteLine("Enter the last name = ");
+            //string lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter the valid email = ");
+            string email = Console.ReadLine();
+
             UserRegistrationRegex userRegistrationRegex = new UserRegistrationRegex();
-            Console.WriteLine(userRegistrationRegex.validateFirstName(firstName));
-            Console.WriteLine(userRegistrationRegex.validatelastName(lastName));
+            //Console.WriteLine(userRegistrationRegex.validateFirstName(firstName));
+            //Console.WriteLine(userRegistrationRegex.validatelastName(lastName));
+            Console.WriteLine(userRegistrationRegex.validatEmail(email));
+
 
 
         }
