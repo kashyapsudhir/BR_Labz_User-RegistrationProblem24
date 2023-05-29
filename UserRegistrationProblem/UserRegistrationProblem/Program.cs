b673aff9fbe4cd7bca4ac464fd4b855bbeb8f4a6
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-05-29 18:19:52 +0530

    [welcome to user registration Problem day24 start assignment]

---
+++ UserRegistrationProblem/UserRegistrationProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationProblem
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("welcome to UserRegistrationProblem day 24 assignment");
+        }
+    }
+}
