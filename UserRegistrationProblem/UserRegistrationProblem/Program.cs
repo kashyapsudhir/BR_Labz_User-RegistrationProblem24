317f388e54a482ecf3cfbbf776525a972bcf0456
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-05-30 16:27:30 +0530

    [check Phone number regex formate or not]

--- UserRegistrationProblem/UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/UserRegistrationProblem/Program.cs
@@ -16,13 +16,23 @@ namespace UserRegistrationProblem
             //Console.WriteLine("Enter the last name = ");
             //string lastName = Console.ReadLine();
 
-            Console.WriteLine("Enter the valid email = ");
-            string email = Console.ReadLine();
+            // Console.WriteLine("Enter the valid email = ");
+            //string email = Console.ReadLine();
+
+            Console.WriteLine("Enter your Phone number, and check valid or not");
+            Console.WriteLine("Enter the mobile number this Pattern=\n [0-9]{2}[ ][0-9]{10}$ ");
+            
+            
 
             UserRegistrationRegex userRegistrationRegex = new UserRegistrationRegex();
             //Console.WriteLine(userRegistrationRegex.validateFirstName(firstName));
             //Console.WriteLine(userRegistrationRegex.validatelastName(lastName));
-            Console.WriteLine(userRegistrationRegex.validatEmail(email));
+            //Console.WriteLine(userRegistrationRegex.validatEmail(email));
+            Console.WriteLine(userRegistrationRegex.validateMobileFormat);
+
+            
+            string TakeNumber = Console.ReadLine();
+            userRegistrationRegex.validateMobileFormat(TakeNumber);
 
 
 
--- UserRegistrationProblem/UserRegistrationProblem/UserRegistrationRegex.cs
+++ UserRegistrationProblem/UserRegistrationProblem/UserRegistrationRegex.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
 
 namespace UserRegistrationProblem
@@ -11,26 +12,38 @@ namespace UserRegistrationProblem
     class UserRegistrationRegex
     {
         //public static string VALIDNAMEPATTERN = "^[A-Z]{1}[a-z]{2,}$"; //UC-1
-        public static string EMAILPATTERN = "^[0-9a-zA-z]+([._+-][0-9a-zA-z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$";
-        
-        
+        //public static string EMAILPATTERN = "^[0-9a-zA-z]+([._+-][0-9a-zA-z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$";
+        public static string PhoneNumberRules = "^[0-9]{2}[ ][0-9]{10}$";
+
+
+
         //UC-1
         //public bool validateFirstName(String firstName)
         //{
         //    return Regex.IsMatch(firstName, VALIDNAMEPATTERN);
         //}
 
-        
+
         //UC-2
         //public bool validatelastName(String firstName)
         //{
         //    return Regex.IsMatch(firstName, VALIDNAMEPATTERN);
         //}
 
-       //UC-3
-        public bool validatEmail(String email)
+        //UC-3
+        //public bool validatEmail(String email)
+        //{
+        //    return Regex.IsMatch(email, EMAILPATTERN);
+        //}
+
+        //UC4
+        public void validateMobileFormat(String PhoneNumber)
         {
-            return Regex.IsMatch(email, EMAILPATTERN);
+            if (Regex.IsMatch(PhoneNumber, PhoneNumberRules))
+                Console.WriteLine($"This is Valid PhoneNumber:{PhoneNumber}");
+            else
+                Console.WriteLine($"This is Invalid PhoneNumber:{PhoneNumber}");
         }
+
     }
 }
